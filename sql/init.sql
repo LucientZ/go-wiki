c6d13309add5ec70264ee5dde069e22befcf46ec
LucientZ <koblenzpoisiden@gmail.com>
2024-07-30 08:11:14 -0500

    Fixed sql INT to INTEGER

--- sql/init.sql
+++ sql/init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS article (
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    id INT PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
 CREATE TABLE IF NOT EXISTS tag (
     tag_name TEXT NOT NULL,
-    article_id INT NOT NULL,
+    article_id INTEGER NOT NULL,
     FOREIGN KEY(article_id) REFERENCES article(id)
 );
